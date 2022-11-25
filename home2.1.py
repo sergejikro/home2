d37e0e84295c81a226a4f827189add96ca6ee25c
Sergey <118309691+sergejikro@users.noreply.github.com>
2022-11-25 19:12:42 +0300

    Add files via upload

---
+++ home2.1.py
@@ -0,0 +1,22 @@
+a = int(input("Введите целое число:"))
+if a % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
+    
+    
+#второе условие
+    
+a = 7
+b = 11
+c = 4
+m = max(a,b,c)
+print(m)
+
+#третье условие
+for i in range(0,10):
+    print(i+1,'0')
+    #четвертое условие
+for i in range(66,9999):
+    if i%7 == 0:
+        print(i)
